f4c0d4f607353ef35265b41019c285890df8caa5
Friedrich C. Kischkel <friedrich.kischkel@gmail.com>
2025-04-26 00:31:41 +0200

    Back to writing PS1 ourselves
    Seems bash doesn't know how much space to reserve for a variable. Better expand
    all the variables upfront and show bash only the result.

--- bashrc.d/prompt.sh
+++ bashrc.d/prompt.sh
@@ -6,44 +6,41 @@ source /usr/share/git/completion/git-prompt.sh
 
 VIRTUAL_ENV_DISABLE_PROMPT=1
 
-function __preamble_ps1() {
+function __folding_ps1() {
     local _errorcode=$?
 
-    if [[ ${_errorcode} -ne 0 ]]; then
-        __error_ps1="🙀${_errorcode} "
-    else
-        __error_ps1=""
+    PS1="\w"
+
+    # user name and host if different from defaults
+    local _host
+    _host=$(hostname)
+    if [[ $USER != "${_bashrc_d_install_user:-}" || $_host != "${_bashrc_d_install_host:-}" ]]; then
+        PS1="\u@\h:${PS1}"
     fi
 
     local _num_jobs
     _num_jobs=$( jobs | wc -l )
     if [[ ${_num_jobs} -ne 0 ]]; then
-        __jobs_ps1="⚒ ${_num_jobs} "
-    else
-        __jobs_ps1=""
+        PS1="\[\e[0;34m\]⚒ \j\[\e[0m\] ${PS1}"
+    fi
+
+    if [[ ${_errorcode} -ne 0 ]]; then
+        PS1="🙀\[\e[1;31m\]${_errorcode}\[\e[0m\] ${PS1}"
     fi
 
     # Python virtual environment?
     if [ -v VIRTUAL_ENV_PROMPT ]; then
-        __venv_ps1="🐍${VIRTUAL_ENV_PROMPT} "
-    else
-        __venv_ps1=""
+       PS1="🐍\[\e[0;32m\]${VIRTUAL_ENV_PROMPT}\[\e[0m\] ${PS1}"
     fi
 
-    # user name and host if different from defaults
-    local _host
-    _host=$(hostname)
-    unset __user_ps1="${USER}@${_host}:"
-    if [[ $USER != "${_bashrc_d_install_user:-}" || $_host != "${_bashrc_d_install_host:-}" ]]; then
-        __user_ps1="${USER}@${_host}:"
-    else
-        __user_ps1=""
-    fi
+    PS1="${VIM:+✌ }${PS1}\[\e[0;32m\]$(__git_ps1)\[\e[0m\]\$ "
+
+    # xterm title bar
+    PS1="\[\e]0;\u@\h: \w\a\]${PS1}"
 
     return ${_errorcode}
 }
 
-PROMPT_COMMAND=__preamble_ps1
+PROMPT_COMMAND=__folding_ps1
 
-#PS1='${VIM:+✌ }${__error_ps1:-}${__jobs_ps1:-}${__venv_ps1:-}${__user_ps1:-\u@\h:}\w$(__git_ps1)\$ '
-PS1='\[\e]0;\u@\h: \w\a\]${VIM:+✌ }\[\e[1;31m\]${__error_ps1:-}\[\e[0m\e[0;34m\]${__jobs_ps1:-}\[\e[0m\e[0;32m\]${__venv_ps1:-}\[\e[0m\]${__user_ps1:-}\w\[\e[0;32m\]$(__git_ps1)\[\e[0m\]\$ '
+PS1="\u@\h:\w\$ "
