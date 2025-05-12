83b5808dfb773b0809023d9bca37926ce2a52a54
Friedrich C. Kischkel <friedrich.kischkel@gmail.com>
2025-05-12 23:31:31 +0200

    Setup Git, initial draft

---
+++ init_git.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# git config set --global user.name
+# git config set --global user.email
+
+git config set --global alias.new "switch -c"
+git config set --global alias.st status
+git config set --global alias.sw sw
