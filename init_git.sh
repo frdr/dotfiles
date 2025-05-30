f9de223062f3a78cade83aa3ab790568318ce66b
Friedrich C. Kischkel <friedrich.kischkel@gmail.com>
2025-05-30 23:30:58 +0200

    More global Git options
    Alias for mergetool and graphical mergetool.
    Only do fast forward pulls (so I'll finally pull again).

--- init_git.sh
+++ init_git.sh
@@ -3,6 +3,14 @@
 # git config set --global user.name
 # git config set --global user.email
 
+git config set --global init.defaultBranch main
+git config set --global pull.ff only
+git config set --global alias.last "diff HEAD^..HEAD"
+git config set --global alias.mg mergetool
 git config set --global alias.new "switch -c"
 git config set --global alias.st status
 git config set --global alias.sw sw
+
+if command -v meld > /dev/null; then
+    git config set --global alias.gmg "mergetool --tool=meld"
+fi
